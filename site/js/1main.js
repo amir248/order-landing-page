762c096d0ea6c183d83ff6b0114082e3431d579b
chikchicly <chikchicly@gmail.com>
2025-03-10 12:46:12 +0700

    setInterval

--- site/js/1main.js
+++ site/js/1main.js
@@ -5,11 +5,39 @@ function first(){
             ok.src="site/js/menu.js";
             document.querySelector('body').append(ok);
         }
-        resolve(loadFirst());
+        setTimeout(()=>{
+            resolve(loadFirst());
+        },1100);
     });
 };//first
+function includeHtml(){
+    return new Promise((resolve)=>{
+        function include(){
+            let newInclude=document.createElement('script');
+            newInclude.src='site/js/includeHtml.js';
+            document.querySelector('body').append(newInclude);
+        }
+        resolve(include());
+    });
+};//includeHtml
+function three(){
+    return new Promise((resolve)=>{
+        function toGo(){
+            setTimeout(()=>{
+                document.querySelector('body > main > header > nav').style.cssText=`
+                opacity:1;
+                transition: all 3s ease-out;
+                `;
+            },1);
+        }
+        resolve(toGo());
+    });
+};//three
 
 async function main(){
+    await includeHtml();
     await first();
+    await three();
+
 };
 main();
