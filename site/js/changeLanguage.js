39331bc5dfe6b66e65d04a697385bca90b4101f9
chikchicly <chikchicly@gmail.com>
2025-03-12 18:18:16 +0700

    console.log(

--- site/js/changeLanguage.js
+++ site/js/changeLanguage.js
@@ -2,7 +2,7 @@ let rusAll=document.querySelectorAll('#rus');
 let rus= document.querySelector('#rus');
 let engAll=document.querySelectorAll('#eng');
 let eng=document.querySelector('#eng');
-console.log(rusAll.length + " "+engAll.length);
+// console.log(rusAll.length + " "+engAll.length);
 let checkbox=document.querySelector("#LanguageCheck");
 document.querySelector("#LanguageCheck").addEventListener("click",()=>{
     // console.log('click');
@@ -12,7 +12,7 @@ document.querySelector("#LanguageCheck").addEventListener("click",()=>{
 function changeLanguageNow(){
     for(let j=0;j<engAll.length;j++){
         if(checkbox.checked==true){
-            console.log('Checkbox Checked');
+            // console.log('Checkbox Checked');
             if(rus!==undefined){
                 rusAll[j].style.display="none";
              }
@@ -22,7 +22,7 @@ function changeLanguageNow(){
             //  checkbox.checked='true';
             
         }else if(checkbox.checked==false){
-            console.log('NOT');
+            // console.log('NOT');
             if(rus!==undefined){
                 rusAll[j].style.display="block";
              }
@@ -38,19 +38,19 @@ changeLanguageNow();
 
 function changeCheckbox(event){
     if(window.location.search=='?rus'){
-        console.log('rus');
+        // console.log('rus');
         checkbox.checked=false;
         changeLanguageNow();
         // event.preventDefault();
         // window.location.search="";
 
     }else if(window.location.search=="?eng"){
-        console.log('eng');
+        // console.log('eng');
         changeLanguageNow();
         // event.preventDefault();
         // window.location.search="?rus";
     }else{
-        console.log('else');
+        // console.log('else');
         changeLanguageNow();
     }
     // console.log(window.location.href);
